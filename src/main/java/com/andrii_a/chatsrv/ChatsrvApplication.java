60d3d7a9d1663c4c4c1df522ed98eba8f157fa7f
Andrii Andrushchenko <aw.andrushchenko@gmail.com>
2024-06-05 00:07:43 +0200

    Initial project setup

---
+++ src/main/java/com/andrii_a/chatsrv/ChatsrvApplication.java
@@ -0,0 +1,13 @@
+package com.andrii_a.chatsrv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatsrvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatsrvApplication.class, args);
+	}
+
+}
